8cfa2e62ce6a874abad5a5d11b6cbd51e409fca5
maitatduy <maitatduy.work@gmail.com>
2025-10-22 03:02:38 +0700

    chore: add Pug and configure views directory

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+// Chỉ định thư mục chưa views
+app.set("views", path.join(__dirname, "views"));
+// Chỉ định loại view engine
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
